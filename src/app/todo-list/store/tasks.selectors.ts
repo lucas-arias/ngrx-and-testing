fa4b49c6c375742677c9b2ae3cce37276e45df46
Lucas Arias <lucas.arias@distillery.com>
2022-07-13 12:37:05 -0300

    update ng rx 14

--- src/app/todo-list/store/tasks.selectors.ts
+++ src/app/todo-list/store/tasks.selectors.ts
@@ -2,6 +2,5 @@ import { createFeatureSelector } from '@ngrx/store';
 import { AppState } from 'src/app/app.state';
 
 export const selectTasks = createFeatureSelector<
-  AppState,
   ReadonlyArray<string>
 >('tasks');
